bff1cab386945a2399954608ba07b6df4f173a84
root <root@NOTEBOOK-OZ>
2023-04-17 15:35:29 +0100

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import 'mdb-ui-kit/css/mdb.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./src/styles/global.css";
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,18 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/static/fonts/`
+      }
+    }
+  ],
 }
 
 export default config
